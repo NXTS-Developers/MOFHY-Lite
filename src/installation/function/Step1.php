200790d650e74501d457f333c1d7b90460516fac
Mahtab Hassan <50697282+mahtab2003@users.noreply.github.com>
2021-09-29 14:55:12 +0500

    Add files via upload

---
+++ src/installation/function/Step1.php
@@ -0,0 +1,37 @@
+<?php
+ob_start();
+session_start();
+if(isset($_POST['submit'])){
+	include __DIR__."/config.php";
+	$connect = mysqli_connect(
+	$DataBase['hostname'],
+	$DataBase['username'],
+	$DataBase['password'],
+	$DataBase['name']
+	);
+	$FormData = array(
+    'name' => $_POST['name'],
+    'url' => $_POST['url'],
+    'email' => $_POST['email'],
+	);
+	$sql = mysqli_query($connect,"INSERT INTO `hosting_area`(`area_key`,`area_name`,`area_url`,`area_email`,`area_status`) VALUES ('AREA','".$FormData['name']."','".$FormData['url']."','".$FormData['email']."','1')");
+	if($sql){
+		$_SESSION['message'] = '<div class="alert alert-success" role="alert">
+									  <button class="close" data-dismiss="alert" type="button" aria-label="Close">
+									    <span aria-hidden="true">&times;</span>
+									  </button>
+									  Clientarea updated <b>successfully!</b>
+									</div>';
+		header('location: ../install.php?step=2');
+	}
+	else{
+		$_SESSION['message'] = '<div class="alert alert-danger" role="alert">
+									  <button class="close" data-dismiss="alert" type="button" aria-label="Close">
+									    <span aria-hidden="true">&times;</span>
+									  </button>
+									  Something went'."'".'s <b>wrong!</b>
+									</div>';
+		header('location: ../install.php?step=1');
+	}
+}
+?>
---
+++ src/installation/function/Step2.php
@@ -0,0 +1,39 @@
+<?php
+ob_start();
+session_start();
+if(isset($_POST['submit'])){
+	include __DIR__."/config.php";
+	$connect = mysqli_connect(
+	$DataBase['hostname'],
+	$DataBase['username'],
+	$DataBase['password'],
+	$DataBase['name']
+	);
+	$FormData = array(
+    'fname' => $_POST['first'],
+    'lname' => $_POST['last'],
+    'password' => sha1($_POST['password']),
+    'email' => $_POST['email'],
+	);
+	$sql = mysqli_query($connect,"INSERT INTO `hosting_admin` (`admin_id`, `admin_fname`, `admin_lname`, `admin_email`, `admin_key`, `admin_password`) VALUES
+(1, '".$FormData['fname']."', '".$FormData['lname']."', '".$FormData['email']."', 999999, '".$FormData['password']."')");
+	if($sql){
+		$_SESSION['message'] = '<div class="alert alert-success" role="alert">
+									  <button class="close" data-dismiss="alert" type="button" aria-label="Close">
+									    <span aria-hidden="true">&times;</span>
+									  </button>
+									  Admin registered <b>successfully!</b>
+									</div>';
+		header('location: ../install.php?step=3');
+	}
+	else{
+		$_SESSION['message'] = '<div class="alert alert-danger" role="alert">
+									  <button class="close" data-dismiss="alert" type="button" aria-label="Close">
+									    <span aria-hidden="true">&times;</span>
+									  </button>
+									  Something went'."'".'s <b>wrong!</b>
+									</div>';
+		header('location: ../install.php?step=2');
+	}
+}
+?>
