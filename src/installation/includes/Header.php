5b6fd570864974700af3b3f2720c59519d0e73aa
Mahtab Hassan <50697282+mahtab2003@users.noreply.github.com>
2021-09-29 14:57:18 +0500

    Add files via upload

---
+++ src/installation/includes/Header.php
@@ -0,0 +1,21 @@
+<?php 
+ob_start();
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Meta tags -->
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+    <meta content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" name="viewport" />
+    <meta name="viewport" content="width=device-width" />
+
+    <link rel="icon" href="../assets/image/fav.png">
+    <title><?php echo $PageInfo['title'];?> - MOFHY</title>
+    <link href="../assets/css/halfmoon.min.css" rel="stylesheet" />
+    <link href="../assets/css/all.min.css" rel="stylesheet" />
+    <link href="../assets/css/style.css" rel="stylesheet" />
+    <link href="../assets/css/login.css" rel="stylesheet" />
+  </head>
+    <body class="with-custom-webkit-scrollbars with-custom-css-scrollbars" data-dm-shortcut-enabled="true" data-sidebar-shortcut-enabled="true" data-set-preferred-theme-onload="true">
