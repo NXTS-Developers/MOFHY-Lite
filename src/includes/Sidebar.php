525aebcfc4228fe5d6a2fcff6d1d8e3bd951fddc
Mahtab Hassan <50697282+mahtab2003@users.noreply.github.com>
2021-09-29 13:16:22 +0500

    Add files via upload

---
+++ src/includes/Sidebar.php
@@ -0,0 +1,60 @@
+<div class="sidebar-overlay" onclick="halfmoon.toggleSidebar()"></div>
+      <div class="sidebar">
+        <div class="sidebar-menu">
+        	<h5 class="sidebar-title">Logged in as:</h5>
+		    <div class="sidebar-divider"></div>
+		    <a href="<?php echo $AreaInfo['area_url'];?>myprofile.php" class="sidebar-link sidebar-link-with-icon">
+		      <span class="sidebar-icon bg-secondary text-dark rounded-circle">
+		        <!--<i class="fa fa-user-circle" aria-hidden="true"></i>-->
+		        <?php $Email = $ClientInfo['hosting_client_email'];
+				$Default = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Twemoji_1f600.svg/440px-Twemoji_1f600.svg.png";
+				$Size = 30;
+				$Grav_URL = "https://www.gravatar.com/avatar/".md5(strtolower(trim($Email)))."?d=".urlencode($Default)."&s=".$Size;
+				?>
+				 <img class="rounded-circle" src="<?php echo $Grav_URL;?>" height="30px" width="30px">
+		      </span>
+		      <?php echo $ClientInfo['hosting_client_fname']." ".$ClientInfo['hosting_client_lname'];?>
+		    </a>
+
+        	<h5 class="sidebar-title">Main Menu</h5>
+		    <div class="sidebar-divider"></div>
+		    <a href="<?php echo $AreaInfo['area_url'];?>index.php" class="sidebar-link sidebar-link-with-icon">
+		      <span class="sidebar-icon bg-transparent">
+		        <i class="fa fa-home" aria-hidden="true"></i>
+		      </span>
+		      Home
+		    </a>
+		    <a href="<?php echo $AreaInfo['area_url'];?>myaccounts.php" class="sidebar-link sidebar-link-with-icon">
+		      <span class="sidebar-icon bg-transparent">
+		        <i class="fa fa-server" aria-hidden="true"></i>
+		      </span>
+		      Accounts
+		    </a>
+		    <a href="<?php echo $AreaInfo['area_url'];?>myssl.php" class="sidebar-link sidebar-link-with-icon">
+		      <span class="sidebar-icon bg-transparent">
+		        <i class="fa fa-lock" aria-hidden="true"></i>
+		      </span>
+		      SSL Certificates
+		    </a>
+		    <a href="<?php echo $AreaInfo['area_url'];?>mytickets.php" class="sidebar-link sidebar-link-with-icon">
+		      <span class="sidebar-icon bg-transparent">
+		        <i class="fa fa-bullhorn" aria-hidden="true"></i>
+		      </span>
+		      Support Tickets
+		    </a>
+		    <a href="<?php echo $AreaInfo['area_url'];?>mysettings.php" class="sidebar-link sidebar-link-with-icon">
+		      <span class="sidebar-icon bg-transparent">
+		        <i class="fa fa-cog" aria-hidden="true"></i>
+		      </span>
+		      Settings
+		    </a>
+		    <a href="<?php echo $AreaInfo['area_url'];?>logout.php" class="sidebar-link sidebar-link-with-icon">
+		      <span class="sidebar-icon bg-transparent">
+		        <i class="fa fa-arrow-circle-right" aria-hidden="true"></i>
+		      </span>
+		      Logout
+		    </a>
+		    <br />
+        </div>
+      </div>
+      <div class="content-wrapper">
