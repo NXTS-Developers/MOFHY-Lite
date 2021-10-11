953c0b1ce1cb800f7282824eb6b64e0530b841af
Mahtab Hassan <50697282+mahtab2003@users.noreply.github.com>
2021-10-11 15:30:38 +0500

    Add files via upload

--- src/includes/Sidebar.php
+++ src/includes/Sidebar.php
@@ -4,14 +4,14 @@
         	<h5 class="sidebar-title">Logged in as:</h5>
 		    <div class="sidebar-divider"></div>
 		    <a href="<?php echo $AreaInfo['area_url'];?>myprofile.php" class="sidebar-link sidebar-link-with-icon">
-		      <span class="sidebar-icon bg-secondary text-dark rounded-circle">
+		      <span class="sidebar-icon bg-transparent text-dark rounded-circle">
 		        <!--<i class="fa fa-user-circle" aria-hidden="true"></i>-->
 		        <?php $Email = $ClientInfo['hosting_client_email'];
 				$Default = "https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/Twemoji_1f600.svg/440px-Twemoji_1f600.svg.png";
 				$Size = 30;
 				$Grav_URL = "https://www.gravatar.com/avatar/".md5(strtolower(trim($Email)))."?d=".urlencode($Default)."&s=".$Size;
 				?>
-				 <img class="rounded-circle" src="<?php echo $Grav_URL;?>" height="30px" width="30px">
+				 <img class="rounded-circle" src="<?php echo $Grav_URL ?? 'Hello';?>" height="30px" width="30px">
 		      </span>
 		      <?php echo $ClientInfo['hosting_client_fname']." ".$ClientInfo['hosting_client_lname'];?>
 		    </a>
@@ -34,13 +34,25 @@
 		      <span class="sidebar-icon bg-transparent">
 		        <i class="fa fa-lock" aria-hidden="true"></i>
 		      </span>
-		      SSL Certificates
+		      SSL Certificate
+		    </a>
+		    <a href="<?php echo $AreaInfo['area_url'];?>knowledgebase.php" class="sidebar-link sidebar-link-with-icon">
+		      <span class="sidebar-icon bg-transparent">
+		        <i class="fa fa-book" aria-hidden="true"></i>
+		      </span>
+		      Knowledgebase
 		    </a>
 		    <a href="<?php echo $AreaInfo['area_url'];?>mytickets.php" class="sidebar-link sidebar-link-with-icon">
 		      <span class="sidebar-icon bg-transparent">
 		        <i class="fa fa-bullhorn" aria-hidden="true"></i>
 		      </span>
-		      My Tickets
+		      Support Ticket
+		    </a>
+		    <a href="<?php echo $AreaInfo['area_url'];?>mytools.php" class="sidebar-link sidebar-link-with-icon">
+		      <span class="sidebar-icon bg-transparent">
+		        <i class="fa fa-tools" aria-hidden="true"></i>
+		      </span>
+		      Extra Tools
 		    </a>
 		    <a href="<?php echo $AreaInfo['area_url'];?>mysettings.php" class="sidebar-link sidebar-link-with-icon">
 		      <span class="sidebar-icon bg-transparent">
