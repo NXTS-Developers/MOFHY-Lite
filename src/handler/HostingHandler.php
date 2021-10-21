44e56f7b66b05403632e3f72863b4b07365cc188
Mahtab Hassan <50697282+mahtab2003@users.noreply.github.com>
2021-10-21 08:00:57 +0500

    Update HostingHandler.php

--- src/handler/HostingHandler.php
+++ src/handler/HostingHandler.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__.'/../includes/Connect.php';
+require __DIR__.'/../includes/Connect.php';
 $sql = mysqli_query($connect,"SELECT * FROM `hosting_account_api` WHERE `api_key`='MOFHAPI'");
 $HostingApi = mysqli_fetch_Assoc($sql);
 ?>
