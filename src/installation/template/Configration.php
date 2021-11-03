e2e9ca20d5865e955d1c52333b1eb41739f0a029
Jai Krishna Tavva <87015531+jaikrishna-tavva9@users.noreply.github.com>
2021-11-03 17:56:08 +0530

    Update Configration.php

--- src/installation/template/Configration.php
+++ src/installation/template/Configration.php
@@ -2,23 +2,23 @@
 	<div class="row">
 		<div class="col-md-6 offset-md-3 col-lg-4 offset-lg-4">
 			<div class="card mx-30" style="opacity: 80%">
-				<h5 class="text-center my-0">Configration</h5><hr>
+				<h5 class="text-center my-0">Configuration</h5><hr>
 				<form action="function/Configration.php" method="post">
 					<div class="mb-5">
 						<label class="form-label">Database Hostname</label>
-						<input type="text" name="hostname" class="form-control" placeholder="Enter database hostname here...">
+						<input type="text" name="hostname" class="form-control" placeholder="(Your database hostname here. Usually "localhost".)">
 					</div>
 					<div class="mb-5">
 						<label class="form-label">Database Username</label>
-						<input type="text" name="username" class="form-control" placeholder="Enter database username here...">
+						<input type="text" name="username" class="form-control" placeholder="(Your database user here.)">
 					</div>
 					<div class="mb-5">
 						<label class="form-label">Database Password</label>
-						<input type="password" name="password" class="form-control" placeholder="Enter database password here...">
+						<input type="password" name="password" class="form-control" placeholder="(Your database user password here.)">
 					</div>
 					<div class="mb-10">
 						<label class="form-label">Database Name</label>
-						<input type="text" name="name" class="form-control" placeholder="Enter database name here...">
+						<input type="text" name="name" class="form-control" placeholder="(Your database name here.)">
 					</div>
 					<div class="mb-0">
 						<input type="submit" name="submit" class="btn btn-primary mb-0" value="Validate">
