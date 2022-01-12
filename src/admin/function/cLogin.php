dd6acc9bd1411031fccda43e68d772b0086f5934
Mahtab Hassan <50697282+mahtab2003@users.noreply.github.com>
2022-01-12 12:47:06 +0500

    Add files via upload

--- src/admin/function/cLogin.php
+++ src/admin/function/cLogin.php
@@ -1,10 +1,11 @@
 <?php
 require __DIR__.'/Connect.php';
+require __DIR__.'/../SessionHandler.php';
 if(isset($_GET['client_id'])){
 	$sql = mysqli_query($connect,"SELECT * FROM `hosting_clients` WHERE `hosting_client_key`='".$_GET['client_id']."'");
 	if(mysqli_num_rows($sql)>0){
 		$Data = mysqli_fetch_assoc($sql);
-		setcookie('LEFSESS',base64_encode($Data['hosting_client_email']),time()+86400,'/');
+		setcookie('LEFSESS',base64_encode($Data['hosting_client_key']),time()+86400,'/');
 			$_SESSION['message'] = '<div class="alert alert-success" role="alert">
 									  <button class="close" data-dismiss="alert" type="button" aria-label="Close">
 									    <span aria-hidden="true">&times;</span>
