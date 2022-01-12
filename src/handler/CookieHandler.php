d0bc419f051dbf684bd7c770a255f67429ebfd8a
Mahtab Hassan <50697282+mahtab2003@users.noreply.github.com>
2022-01-12 12:44:59 +0500

    Add files via upload

--- src/handler/CookieHandler.php
+++ src/handler/CookieHandler.php
@@ -1,11 +1,11 @@
 <?php
 if(isset($_COOKIE['LEFSESS'])){
-	$sql = mysqli_query($connect,"SELECT * FROM `hosting_clients` WHERE `hosting_client_email`='".base64_decode($_COOKIE['LEFSESS'])."'");
+	$sql = mysqli_query($connect,"SELECT * FROM `hosting_clients` WHERE `hosting_client_key`='".base64_decode($_COOKIE['LEFSESS'])."'");
 	if(mysqli_num_rows($sql)>0){
-		$ClientInfo = mysqli_fetch_Assoc($sql);
+		$ClientInfo = mysqli_fetch_assoc($sql);
 	}
 	else{
-		setcookie('LEFSESS','NULL',-1,'/');
+		setcookie('LEFSESS', "null" ,-1,'/');
 		$_SESSION['message'] = '<div class="alert alert-danger" role="alert">
 									  <button class="close" data-dismiss="alert" type="button" aria-label="Close">
 									    <span aria-hidden="true">&times;</span>
