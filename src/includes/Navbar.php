fbe73dfe6bee48e0bfceeb42470e7fb8923d5293
Fury_Craft <44116544+FuryCraft@users.noreply.github.com>
2022-02-24 18:39:59 +0100

    Changed button to switch between themes

--- src/includes/Navbar.php
+++ src/includes/Navbar.php
@@ -3,6 +3,9 @@
         <div class="container-fluid">
         	<a href="<?php echo $AreaInfo['area_url'];?>index.php" class="navbar-brand"><?php echo $AreaInfo['area_name'];?></a>
         	<ul class="navbar-nav ml-auto">
+		    <li class="nav-item nav-height">
+		      <button class="btn btn-sm my-auto" role="button" onclick="halfmoon.toggleDarkMode()"><i class="fas fa-paint-roller"></i></button>
+            	    </li>
 		    <li class="nav-item nav-height dropdown with-arrow">
 		      <a class="btn btn-sm m5x" data-toggle="dropdown" id="nav-link-dropdown-toggle">
 		        <i class="fa fa-user"></i>
@@ -11,10 +14,6 @@
 		        <a href="<?php echo $AreaInfo['area_url'];?>myprofile.php" class="dropdown-item">Profile</a>
 		        <a href="<?php echo $AreaInfo['area_url'];?>mysettings.php" class="dropdown-item">Settings</a>
 		        <a href="<?php echo $AreaInfo['area_url'];?>logout.php" class="dropdown-item">Logout</a>
-		        <div class="dropdown-divider"></div>
-		        <div class="dropdown-content">
-		          <button class="btn btn-block" role="button" onclick="halfmoon.toggleDarkMode()"><i class="fa fa-sun"></i> Theme</button>
-		        </div>
 		      </div>
 		    </li>
 		    <li class="nav-item nav-height hidden-on-up">
