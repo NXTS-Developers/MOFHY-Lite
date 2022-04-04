bd39512445e39deff56b362da640217774158607
JAI KRISHNA <87015531+jaikt@users.noreply.github.com>
2022-04-04 15:55:56 +0530

    Replace this <i> tag by <em>

--- src/template/cPLogin.php
+++ src/template/cPLogin.php
@@ -2,7 +2,7 @@
 	<div class="card py-0">
 		<div class="d-flex justify-content-between align-items-center pt-15 mx-5">
 			<h5 class="m-0">cPanel Login</h5>
-			<a href="<?php echo $AreaInfo['area_url'];?>viewaccount.php?account_id=<?php echo $_GET['account_id'];?>" class="btn btn-danger btn-sm"><i class="fa fa-backward"></i> Return</a>
+			<a href="<?php echo $AreaInfo['area_url'];?>viewaccount.php?account_id=<?php echo $_GET['account_id'];?>" class="btn btn-danger btn-sm"><em class="fa fa-backward"></em> Return</a>
 		</div><hr>
 		<?php $AccountInfo = mysqli_fetch_assoc($sql);?>
 		<div class="mb-15">
