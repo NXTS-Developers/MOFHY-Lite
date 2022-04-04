52a55b93aa352b668ad85a0f19292de8396379d4
JAI KRISHNA <87015531+jaikt@users.noreply.github.com>
2022-04-04 16:07:33 +0530

    Update NewTicket.php

--- src/template/NewTicket.php
+++ src/template/NewTicket.php
@@ -2,7 +2,7 @@
 	<div class="card py-0">
 		<div class="d-flex justify-content-between align-items-center pt-15 px-5">
 			<h5 class="m-0">New Ticket</h5>
-			<a href="<?php echo $AreaInfo['area_url'];?>mytickets.php" class="btn btn-sm btn-danger"><i class="fa fa-backward"></i> Return</a>
+			<a href="<?php echo $AreaInfo['area_url'];?>mytickets.php" class="btn btn-sm btn-danger"><em class="fa fa-backward"></em> Return</a>
 		</div>
 		<hr>
 		<form action="function/NewTicket.php" method="post">
