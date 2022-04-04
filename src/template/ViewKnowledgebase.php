f94e3386585815f972285e986b31129b3b294ac7
JAI KRISHNA <87015531+jaikt@users.noreply.github.com>
2022-04-04 16:01:37 +0530

    Replace this <b> tag by <strong>

--- src/template/ViewKnowledgebase.php
+++ src/template/ViewKnowledgebase.php
@@ -2,20 +2,20 @@
 	<div class="card py-0">
 		<div class="d-flex justify-content-between align-items-center pt-15">
 			<h5 class="m-0">Viewing Knowledgebase #<?php echo htmlspecialchars($_GET['knowledgebase_id']) ?></h5>
-			<a href="<?php echo $AreaInfo['area_url'];?>knowledgebase.php" class="btn btn-sm btn-danger"><i class="fa fa-backward"></i> Return</a>
+			<a href="<?php echo $AreaInfo['area_url'];?>knowledgebase.php" class="btn btn-sm btn-danger"><em class="fa fa-backward"></em> Return</a>
 		</div><hr>
 		<?php $Knowledgebase = mysqli_fetch_assoc($sql);?>
 		<div class="mb-20">
 			<div class="row">
 				<div class="col-md-6">
 					<div class="d-flex justify-content-between align-items-center mx-10 my-5">
-						<b>Subject:</b>
+						<strong>Subject:</strong>
 						<span><?php echo $Knowledgebase['knowledgebase_subject'];?></span>
 					</div>
 				</div>
 				<div class="col-md-6">
 					<div class="d-flex justify-content-between align-items-center mx-10 my-5">
-						<b>Date:</b>
+						<strong>Date:</strong>
 						<span><?php echo $Knowledgebase['knowledgebase_date'];?></span>
 					</div>
 				</div>
