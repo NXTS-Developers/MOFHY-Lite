ecfb9d96097b4ca32f33fe9f48a10380a3aaa6ba
JAI KRISHNA <87015531+jaikt@users.noreply.github.com>
2022-04-04 16:04:23 +0530

    Replace this <i> tag by <em>

--- src/template/Suspended.php
+++ src/template/Suspended.php
@@ -5,18 +5,18 @@
         	<ul class="navbar-nav ml-auto">
 		    <li class="nav-item nav-height dropdown with-arrow">
 		      <a class="btn btn-sm m5x" data-toggle="dropdown" id="nav-link-dropdown-toggle">
-		        <i class="fa fa-user"></i>
+		        <em class="fa fa-user"></em>
 		      </a>
 		      <div class="dropdown-menu dropdown-menu-right" aria-labelledby="nav-link-dropdown-toggle">
 		        <a href="<?php echo $AreaInfo['area_url'];?>logout.php" class="dropdown-item">Logout</a>
 		        <div class="dropdown-divider"></div>
 		        <div class="dropdown-content">
-		          <button class="btn btn-block" role="button" onclick="halfmoon.toggleDarkMode()"><i class="fa fa-sun"></i> Switch</button>
+		          <button class="btn btn-block" role="button" onclick="halfmoon.toggleDarkMode()"><em class="fa fa-sun"></em> Switch</button>
 		        </div>
 		      </div>
 		    </li>
 		    <li class="nav-item nav-height hidden-on-up">
-		      <button class="btn btn-sm my-auto" onclick="halfmoon.toggleSidebar()"><i class="fa fa-bars"></i></button>
+		      <button class="btn btn-sm my-auto" onclick="halfmoon.toggleSidebar()"><em class="fa fa-bars"></em></button>
 		    </li>
         </div>
       </nav>	
@@ -27,7 +27,7 @@
 		    <div class="sidebar-divider"></div>
 		    <a href="myprofile.php" class="sidebar-link sidebar-link-with-icon">
 		      <span class="sidebar-icon bg-secondary text-dark rounded-circle">
-		        <i class="fa fa-user-circle" aria-hidden="true"></i>
+		        <em class="fa fa-user-circle" aria-hidden="true"></em>
 		      </span>
 		      <?php
 		      if($ClientInfo['hosting_client_status'] == 1)
@@ -41,7 +41,7 @@
 		    <div class="sidebar-divider"></div>
 		    <a href="<?php echo $AreaInfo['area_url'];?>logout.php" class="sidebar-link sidebar-link-with-icon">
 		      <span class="sidebar-icon bg-transparent">
-		        <i class="fa fa-arrow-circle-right" aria-hidden="true"></i>
+		        <em class="fa fa-arrow-circle-right" aria-hidden="true"></em>
 		      </span>
 		      Logout
 		    </a>
