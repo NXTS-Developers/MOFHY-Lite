ed268567e617f2b756e409f516c79938f7567494
Greenreader9 <75644395+greenreader9@users.noreply.github.com>
2022-07-20 09:16:12 -0500

    Update Home.php

--- src/installation/template/Home.php
+++ src/installation/template/Home.php
@@ -5,7 +5,9 @@
 				<div class="text-center">
 					<i class="fa fa-info-circle fa-5x icon-text"></i>
 					<h3 class="my-0">Installation</h3>
-					<p class="my-0 mb-5">Hi there! looks like you have not installed MOFY yetplease click on the button bellow to start installation. <i class="fa fa-smile"></i></p>
+					<p class="my-0 mb-5">Hi there! looks like you have not installed MOFY yet. Click the button bellow to start the installation. <i class="fa fa-smile"></i></p>
+					<p class="my-0 mb-5"><strong><i>Warning!</i> This software is not secure, and client information can be leaked in seconds. Do not use this software in a production or public envirement. Falure the abide by the previous statement may result in legal action by clients when you are hacked.</strong><br><br>
+				        By clicking the button below, you acknowledge that this software in insecure, and release NXTS-Developers, as well as all maiantainers, from any and all legal action.</p>
 					<a href="install.php" class="btn btn-primary">Install Now!</a>
 				</div>
 			</div>
